92e519bffaf46a93bbb11e53fc24cffd6fe0dd55
Timur Kosinov <kosinov.tim@gmail.com>
2025-03-21 00:03:41 +0300

    Initial commit with file path validation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int fileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или путь является папкой. Повторите ввод.");
+            } else {
+                System.out.println("Путь указан верно");
+                fileCount++;
+                System.out.println("Это файл номер " + fileCount);
+            }
+        }
     }
 }
